1bbfe92d61e03617ba906b37f7db1cbcf90c3c2d
Vladyslav Reva <juve1991@gmail.com>
2020-02-28 17:29:25 +0200

    Add prepearing data for athletes and results table

--- importdata.js
+++ importdata.js
@@ -21,7 +21,7 @@ const parseFile = async (file) => {
 const process = async (array) => {
   console.log('Processing...');
   const header = array.shift();
-  const table = header.reduce((curr, key, idx) => { 
+  const table = header.reduce((curr, key, idx) => {
     return curr.set(key, array.map(val => val[idx]));
   }, new Map());
 
@@ -63,6 +63,7 @@ const process = async (array) => {
           season: season,
           city: acc.get(game).city + ', ' + city
         });
+      // skip 1906 Summer games
       } else if (game !== '1906 Summer' && !acc.has(game)) {
         acc.set(game, {
           year: year,
@@ -80,11 +81,98 @@ const process = async (array) => {
     };
   });
 
+  const athletes = Array.from(
+    table.get('ID').reduce((acc, id, idx) => {
+      const getParams = () => {
+        let params = '{';
+        if (table.get('Height')[idx] !== 'NA') {
+          params += ' height: ' + table.get('Height')[idx] + ',';
+        }
+        if (table.get('Weight')[idx] !== 'NA') {
+          params += ' weight: ' + table.get('Weight')[idx] + ' ';
+        }
+        // remove "," in the end of the string
+        return params.replace(/,$/g, ' ') + '}';
+      };
+      const getBirth = () => {
+        if (table.get('Age')[idx] === 'NA' || table.get('Year')[idx] === 'NA') {
+          return 'NULL';
+        } else {
+          return table.get('Year')[idx] - table.get('Age')[idx];
+        }
+      };
+      const getTeamId = () => {
+        const team = teams.find(el => el.noc_name === table.get('NOC')[idx]);
+        return team && team.id;
+      };
+      const getName = () => {
+        // remove any information in round brackets and double quotemarks
+        return table.get('Name')[idx].replace(/(\s\(.+\))/g, '').replace(/(\s"".+"")/g, '');
+      };
+      const getSex = () => {
+        return table.get('Sex')[idx] === 'NA' ? 'NULL' : table.get('Sex')[idx];
+      };
+      return acc.set(id, {
+        name: getName(),
+        sex: getSex(),
+        birth: getBirth(),
+        params: getParams(),
+        team_id: getTeamId()
+      });
+    }, new Map())).map(el => {
+    return {
+      id: +el[0],
+      full_name: el[1].name,
+      year_of_birth: el[1].birth,
+      sex: el[1].sex,
+      params: el[1].params,
+      team_id: el[1].team_id
+    };
+  });
+
+  const results = table.get('ID').reduce((acc, el, idx) => {
+    const getMedal = () => {
+      switch (table.get('Medal')[idx]) {
+      case 'Gold': return 1;
+      case 'Silver':return 2;
+      case 'Bronze': return 3;
+      default: return 0;
+      }
+    };
+    const getGameId = () => {
+      const season = table.get('Season')[idx] === 'Summer' ? 0 : 1;
+      const year = table.get('Year')[idx];
+      const game = games.find(el => el.year === year && el.season === season);
+      return game && game.id;
+    };
+    const getSportId = () => {
+      const sport = sports.find(el => el.name === table.get('Sport')[idx]);
+      return sport && sport.id;
+    };
+    const getEventId = () => {
+      const event = events.find(el => el.name === table.get('Event')[idx]);
+      return event && event.id;
+    };
+    // skip results of 1906 Summer games
+    if (table.get('Games')[idx] !== '1906 Summer') {
+      acc.push({
+        athlete_id: +el,
+        game_id: getGameId(),
+        sport_id: getSportId(),
+        event_id: getEventId(),
+        medal: getMedal()
+      });
+    }
+    return acc;
+  }, []);
+
   return {
     teams: teams,
     sports: sports,
     events: events,
-    games: games
+    games: games,
+    athletes: athletes,
+    results: results
   };
 };
 
